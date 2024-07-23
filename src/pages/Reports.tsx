1c7a5d8039d8b1b20a3ab296142dfa61f848fd86
montaser77789 <montasergohar@gmail.com>
2024-07-23 16:46:36 +0300

    playerAttendance

--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
 import { useParams } from 'react-router-dom';
 
 const Reports = () => {
     const { dayId, monthId } = useParams();
+    console.log(dayId, monthId);
+    
   return (
     <div>
       
